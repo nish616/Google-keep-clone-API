d7bb72485594c4311b8f8be32189066999d8ccf5
nish616 <nishin616@gmail.com>
2021-05-26 00:02:54 +0530

    Fix port

--- app.js
+++ app.js
@@ -42,5 +42,9 @@ try{
 }
 
 finally{
-    app.listen('5000', console.log("Listening at port 5000"));
+    let port = process.env.PORT;
+    if (port == null || port == "") {
+     port = 5000;
+    }
+    app.listen(port, console.log("Listening at port 5000"));
 }
